704522ef25f4523847eb35b297c9acaf0463a4b8
alpha-bird <alpha.birdprime@gmail.com>
2018-06-28 15:58:29 +0300

    remote video component built, multi broadcasting

--- src/App.js
+++ src/App.js
@@ -1,121 +1,20 @@
 import React, { Component } from 'react';
-import './App.css';
+import RemoteVideo from './components/remotevideo';
 import logo from './logo.svg';
-import kurentoUtils from 'kurento-utils';
-
-var ws = new WebSocket('wss://18.232.60.204:8080/one2many')
-var webRtcPeer
-
-ws.onmessage = function(message) {
-	var parsedMessage = JSON.parse(message.data);
-	console.info('Received message: ' + message.data);
-
-	switch (parsedMessage.id) {
-	case 'presenterResponse':
-		presenterResponse(parsedMessage);
-		break;
-	case 'viewerResponse':
-		viewerResponse(parsedMessage);
-		break;
-	case 'stopCommunication':
-		dispose();
-		break;
-	case 'iceCandidate':
-		console.log("******************************************************iceCandidate message");
-		webRtcPeer.addIceCandidate(parsedMessage.candidate)
-		break;
-	default:
-		console.error('Unrecognized message', parsedMessage);
-	}
-}
-function presenterResponse(message) {
-	if (message.response != 'accepted') {
-		var errorMsg = message.message ? message.message : 'Unknow error';
-		console.warn('Call not accepted for the following reason: ' + errorMsg);
-		dispose();
-	} else {
-		webRtcPeer.processAnswer(message.sdpAnswer);
-	}
-}
-
-function viewerResponse(message) {
-	if (message.response != 'accepted') {
-		var errorMsg = message.message ? message.message : 'Unknow error';
-		console.warn('Call not accepted for the following reason: ' + errorMsg);
-		dispose();
-	} else {
-		webRtcPeer.processAnswer(message.sdpAnswer);
-	}
-}
-
-function dispose() {
-	if (webRtcPeer) {
-		webRtcPeer.dispose();
-		webRtcPeer = null;
-	}
-//	hideSpinner(video);
-}
-
-function onIceCandidate(candidate) {
-    console.log('Local candidate' + JSON.stringify(candidate));
-
-    var message = {
-       id : 'onIceCandidate',
-       candidate : candidate
-    }
-    sendMessage(message);
-}
-
-function onOfferViewer(error, offerSdp) {
-    if (error) return console.log(error)
-
-    var message = {
-        id : 'viewer',
-        sdpOffer : offerSdp
-    }
-    sendMessage(message);
-}
-
-function sendMessage(message) {
-    var jsonMessage = JSON.stringify(message);
-    console.log('Senging message: ' + jsonMessage);
-    ws.send(jsonMessage);
-}
+import './App.css';
 
 class App extends Component {
-    componentDidMount() {
-        var _this = this
-        ws.onopen = function() {
-            _this.viewer()
-        }
-    }
-    viewer() {
-        if (!webRtcPeer) {
-            //showSpinner(video);
-
-            var options = {
-                remoteVideo: document.getElementById('video'),
-                onicecandidate : onIceCandidate
-            }
-            
-            webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options, function(error) {
-                if(error) return console.log(error)
-    
-                this.generateOffer(onOfferViewer);
-            });
-        }
-    }
-    
-
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">Welcome to Well Checked</h1>
                 </header>
-                <video id="video" autoPlay width="640px" height="480px" poster="img/webrtc.png"></video>
-                <button onClick = {this.viewer}>Start View</button>
+                <div style = {{ flexDirection : "row", display : "flex" }}>
+                    <RemoteVideo videoId = "video1"/>
+                    <RemoteVideo videoId = "video2"/>
+                </div>
             </div>
         );
     }
---
+++ src/components/remotevideo/index.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import kurentoUtils from 'kurento-utils';
+import './styles.css';
+
+class RemoteVideo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ws : new WebSocket('wss://18.232.60.204:8080/one2many'), 
+            webRtcPeer : null,
+            videoId : props.videoId,
+        }
+    }
+
+    componentDidMount() {
+        var webSocket = this.state.ws
+        var _this = this
+        webSocket.onopen = function() {
+            _this.viewer()
+        }
+        webSocket.onmessage = function(message) {
+            var parsedMessage = JSON.parse(message.data);
+            console.info('Received message: ' + message.data);
+        
+            switch (parsedMessage.id) {
+            case 'presenterResponse':
+                _this.presenterResponse(parsedMessage);
+                break;
+            case 'viewerResponse':
+                _this.viewerResponse(parsedMessage);
+                break;
+            case 'stopCommunication':
+                _this.dispose();
+                break;
+            case 'iceCandidate':
+                console.log("******************************************************iceCandidate message");
+                _this.state.webRtcPeer.addIceCandidate(parsedMessage.candidate)
+                break;
+            default:
+                console.error('Unrecognized message', parsedMessage);
+            }
+        }
+
+        this.setState({
+            ws : webSocket
+        })
+    }
+
+    presenterResponse(message) {
+        if (message.response !== 'accepted') {
+            var errorMsg = message.message ? message.message : 'Unknow error';
+            console.warn('Call not accepted for the following reason: ' + errorMsg);
+            this.dispose();
+        } else {
+            this.state.webRtcPeer.processAnswer(message.sdpAnswer);
+        }
+    }
+    
+    viewerResponse(message) {
+        if (message.response !== 'accepted') {
+            var errorMsg = message.message ? message.message : 'Unknow error';
+            console.warn('Call not accepted for the following reason: ' + errorMsg);
+            this.dispose();
+        } else {
+            this.state.webRtcPeer.processAnswer(message.sdpAnswer);
+        }
+    }
+    
+    dispose() {
+        if (this.state.webRtcPeer) {
+            this.state.webRtcPeer.dispose();
+            this.setState({
+                webRtcPeer : null
+            })
+        }
+    //	hideSpinner(video);
+    }
+    
+    onIceCandidate(candidate, _this) {
+        console.log('Local candidate' + JSON.stringify(candidate));
+    
+        var message = {
+           id : 'onIceCandidate',
+           candidate : candidate
+        }
+        _this.sendMessage(message)
+    }
+    
+    onOfferViewer(error, offerSdp, _this) {
+        if (error) return console.log(error)
+    
+        var message = {
+            id : 'viewer',
+            sdpOffer : offerSdp
+        }
+        _this.sendMessage(message)
+    }
+    
+    sendMessage(message) {
+        var jsonMessage = JSON.stringify(message);
+        console.log('Senging message: ' + jsonMessage);
+        this.state.ws.send(jsonMessage);
+    }
+
+    viewer() {
+        if (!this.state.webRtcPeer) {
+            //showSpinner(video);
+
+            var options = {
+                remoteVideo: document.getElementById(this.state.videoId),
+                onicecandidate : (candidate) => {this.onIceCandidate(candidate, this)}
+            }
+            var _this = this
+            var peer = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options, function(error) {
+                if(error) return console.log(error)
+    
+                this.generateOffer((err, offerSdp) => {_this.onOfferViewer(err, offerSdp, _this)});
+            });
+            this.setState({
+                webRtcPeer : peer
+            })
+        }
+    }
+    
+
+    render() {
+        return (
+            <video id={ this.state.videoId } autoPlay width="640px" height="480px" poster="img/webrtc.png"></video>
+        );
+    }
+}
+
+export default RemoteVideo;
